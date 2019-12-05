7589b7edc57736b04032db6978492ef63da384c9
Vincenzo Abete <abetevincenzo@gmail.com>
2019-12-05 00:30:38 +0000

    First commit. Module test GetOne passes

---
+++ ModuleTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spital;
+
+namespace spitalUnitTests
+{
+    [TestClass]
+    public class ModuleTests
+    {
+        [TestMethod]
+        public void GetOne_Test()
+        {
+            Module module = new Module
+            {
+                Name = "Module name",
+                Icon = "icon",
+                DefaultMin = 1.5F,
+                DefaultMax = 10.5F
+            };
+
+            Nullable<int> insertedID = module.Save();
+
+            Module retrievedModule = new Module();
+
+            retrievedModule = Module.GetOne(insertedID);
+
+            /* 
+             * Cannot assert that the two objects are the same because
+             * the comparison is done by reference. Every property
+             * must be checked to ensure the two objects are the same
+             */
+            Assert.AreEqual(retrievedModule.Id, insertedID);
+            Assert.AreEqual(retrievedModule.Name, module.Name);
+            Assert.AreEqual(retrievedModule.Icon, module.Icon);
+            Assert.AreEqual(retrievedModule.DefaultMin, module.DefaultMin);
+            Assert.AreEqual(retrievedModule.DefaultMax, module.DefaultMax);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("spitalUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("spitalUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8969381-51fe-4999-9043-ac08d8346eae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
